2c665c2f4ddd246fb11af8698bdd56b174897fe2
jeremy <jeremy@scotex.be>
2023-11-14 21:44:57 -0500

    c

--- src/app/grocery-list/grocery-list.service.ts
+++ src/app/grocery-list/grocery-list.service.ts
@@ -1,6 +1,6 @@
 import { HttpClient } from "@angular/common/http";
 import { Injectable, inject } from "@angular/core";
-import { Subject, from, lastValueFrom, map, switchMap } from "rxjs";
+import { Subject, lastValueFrom, map } from "rxjs";
 
 @Injectable({ providedIn: 'root' })
 export class GroceryListService {
